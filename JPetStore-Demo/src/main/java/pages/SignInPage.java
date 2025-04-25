2a04064dfa8edf751d5241b930a41cf69ebb79fb
shhdyasser <shahdyasser108108@gmail.com>
2025-04-25 11:17:14 +0300

    Initial Commit

--- JPetStore-Demo/src/main/java/pages/SignInPage.java
+++ JPetStore-Demo/src/main/java/pages/SignInPage.java
@@ -3,12 +3,18 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+
 public class SignInPage extends  PageBase{
 
     By signIn = By.xpath("//*[@id=\"Menu\"]/div[1]/a[2]");
     By userName = By.name("username");
     By password =By.name("password");
     By loginButton = By.xpath("//*[@id=\"Signon\"]/form/div/div/button");
+    By signOutHome = By.xpath("//a[@href='/account/signoff']");
+    By errorMessage = By.xpath("//div[@class='panel failed']");
+
+    public String signOutText = "Sign Out";
+
 
     public SignInPage(WebDriver driver) {
         super(driver);
@@ -33,5 +39,17 @@ public class SignInPage extends  PageBase{
 
         clickOnElement(loginButton);
     }
-}
 
+    public String ActualResultAssert(){
+        return driver.findElement(signOutHome).getText();
+    }
+
+    public String getErrorMessage() {
+        return driver.findElement(errorMessage).getText();
+    }
+
+    public String getPasswordFieldType() {
+        return driver.findElement(By.name("password")).getAttribute("type");
+    }
+
+}
--- JPetStore-Demo/src/test/java/tests/SignInTest.java
+++ JPetStore-Demo/src/test/java/tests/SignInTest.java
@@ -1,28 +1,80 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.SignInPage;
+import static org.testng.Assert.*;
 
-
-public class SignInTest extends TestBase{
+public class SignInTest extends TestBase {
 
     SignInPage signInPage;
 
     @BeforeMethod
-    public void init(){
+    public void init() {
         signInPage = new SignInPage(driver);
-
     }
 
+
     @Test
-    public void LoginWithValidUsernameAndValidPassword() throws InterruptedException {
-        System.out.println(driver);
+    public void loginWithValidCredentials() {
         signInPage.clickSignIn();
         signInPage.fillUserName("j2ee");
         signInPage.fillPassword("j2ee");
         signInPage.clickOnLoginButton();
-        Thread.sleep(5000);
+
+        Assert.assertEquals(signInPage.ActualResultAssert(),signInPage.signOutText);
+
+    }
+
+
+    @Test
+    public void loginWithInvalidPassword() {
+        signInPage.clickSignIn();
+        signInPage.fillUserName("j2ee");
+        signInPage.fillPassword("wrongpass");
+        signInPage.clickOnLoginButton();
+
+        Assert.assertEquals(signInPage.getErrorMessage(),"Invalid username or password. Signon failed.");
+    }
+
+    @Test
+    public void loginWithNonexistentUser() {
+        signInPage.clickSignIn();
+        signInPage.fillUserName("nonexistent_user");
+        signInPage.fillPassword("anypass");
+        signInPage.clickOnLoginButton();
+
+        Assert.assertEquals(signInPage.getErrorMessage(),"Invalid username or password. Signon failed.");
+    }
+
+
+    @Test
+    public void loginWithEmptyUsername() {
+        signInPage.clickSignIn();
+        signInPage.fillUserName("");
+        signInPage.fillPassword("j2ee");
+        signInPage.clickOnLoginButton();
+
+        Assert.assertEquals(signInPage.getErrorMessage(),"Invalid username or password. Signon failed.");
+    }
+
+    @Test
+    public void loginWithEmptyPassword() {
+        signInPage.clickSignIn();
+        signInPage.fillUserName("j2ee");
+        signInPage.fillPassword("");
+        signInPage.clickOnLoginButton();
+
+        Assert.assertEquals(signInPage.getErrorMessage(),"Invalid username or password. Signon failed.");
+    }
+
+    @Test
+    public void verifyPasswordMasked() {
+        signInPage.clickSignIn();
+        signInPage.fillPassword("secret");
+
+        assertEquals(signInPage.getPasswordFieldType(), "password");
     }
 
 }

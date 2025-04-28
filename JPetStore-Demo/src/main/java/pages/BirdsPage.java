515a700434d67a61faa35416146b5fb9421e08a1
Abdelkader7151 <boudy7151@gmail.com>
2025-04-28 03:35:17 +0300

    cats page commit

--- JPetStore-Demo/src/main/java/pages/BirdsPage.java
+++ JPetStore-Demo/src/main/java/pages/BirdsPage.java
@@ -4,10 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class BirdsPage extends PageBase {
-//    By signIn = By.xpath("//*[@id=\"Menu\"]/div[1]/a[2]");
-//    By userName = By.name("username");
-//    By password =By.name("password");
-//    By loginButton = By.xpath("//*[@id=\"Signon\"]/form/div/div/button");
     By birdsTab = By.xpath("//div[@id='QuickLinks']/a[text()='Birds']");
 
     By amazonParrotTab =  By.xpath("//tr/td/a[text()='AV-CB-01']");
@@ -27,23 +23,6 @@ public class BirdsPage extends PageBase {
     public BirdsPage(WebDriver driver) {
         super(driver);
     }
-//    public void clickSignIn(){
-//        scrollDown("window.scrollBy(0,368.79998779296875)");
-//        clickOnElement(signIn);
-//    }
-//    public void fillUserName(String name){
-//        scrollDown("window.scrollBy(0,368.79998779296875)");
-//        sendKeys(userName,name);
-//    }
-//    public void fillPassword(String password){
-//
-//        sendKeys(this.password,password);
-//    }
-//
-//    public void clickOnLoginButton() {
-//
-//        clickOnElement(loginButton);
-//    }
     public void clickOnBirdsTab(){
         scrollDown("window.scrollBy(0,368.79998779296875)");
         clickOnElement(birdsTab);
--- JPetStore-Demo/src/test/java/tests/CatsTest.java
+++ JPetStore-Demo/src/test/java/tests/CatsTest.java
@@ -1,55 +1,67 @@
 package tests;
 
 import Helper.LoginHelper;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.CatsPage;
 
-public class CatsTest extends TestBase{
+public class CatsTest extends TestBase {
     CatsPage catsPage;
     LoginHelper loginHelper;
 
     @BeforeMethod
-    public void init(){
+    public void init() {
         loginHelper = new LoginHelper(driver);
         catsPage = new CatsPage(driver);
+        loginHelper.login("salem", "Passw0rd123");
     }
 
-    @Test
-    public void CatsPageTesttillCheckout() throws InterruptedException {
-        System.out.println(driver);
-        loginHelper.login("salem","Passw0rd123");
-
+    @Test(priority = 1)
+    public void verifyCatCategoriesLoad() {
         catsPage.clickOncatsTab();
-        Thread.sleep(2000);
+        Assert.assertEquals(catsPage.isCategoryHeaderDisplayed(), "Cats");
+    }
+
+    @Test(priority = 2)
+    public void testManxCatAdoption() {
+        catsPage.clickOncatsTab();
         catsPage.clickOnManxTab();
-        Thread.sleep(2000);
         catsPage.clickOnTaillessManxTab();
-        Thread.sleep(3000);
 
-        catsPage.clickOncatsTab();
-        Thread.sleep(2000);
-        catsPage.clickOnManxTab();
-        Thread.sleep(2000);
-        catsPage.clickOnTailManxTab();
-        Thread.sleep(3000);
+        Assert.assertEquals(catsPage.getCartCount(), "1");
+    }
 
+    @Test(priority = 3)
+    public void testPersianCatAdoption() {
         catsPage.clickOncatsTab();
-        Thread.sleep(2000);
         catsPage.clickOnPersianTab();
-        Thread.sleep(2000);
-        catsPage.clickOnFemalePersianTab();
-        Thread.sleep(3000);
-
-        catsPage.clickOncatsTab();
-        Thread.sleep(2000);
-        catsPage.clickOnPersianTab();
-        Thread.sleep(2000);
         catsPage.clickOnMalePersianTab();
-        Thread.sleep(4000);
+
+        Assert.assertEquals(catsPage.isItemInCart(), "EST-17");
+    }
+
+    @Test(priority = 4)
+    public void testCheckoutProcess() throws InterruptedException {
+        catsPage.clickOncatsTab();
+        catsPage.clickOnManxTab();
+        catsPage.clickOnTailManxTab();
 
         catsPage.clickOnCheckoutTab();
-        Thread.sleep(6000);
+        Thread.sleep(3000);
+        Assert.assertEquals(catsPage.isCheckoutHeaderDisplayed(), "Payment Details");
+    }
 
+    @Test(priority = 5)
+    public void testMultipleItemCart() {
+        catsPage.clickOncatsTab();
+        catsPage.clickOnManxTab();
+        catsPage.clickOnTaillessManxTab();
+
+        catsPage.clickOncatsTab();
+        catsPage.clickOnPersianTab();
+        catsPage.clickOnFemalePersianTab();
+
+        Assert.assertEquals(catsPage.getCartCount(), "2","Cart should contain 2 items");
     }
 }

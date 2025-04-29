8e2b7d49778abc95a75d644d942e649e65315cb0
Abdelkader7151 <boudy7151@gmail.com>
2025-04-29 04:48:52 +0300

    Fish page commit

--- JPetStore-Demo/src/main/java/pages/FishPage.java
+++ JPetStore-Demo/src/main/java/pages/FishPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class FishPage extends PageBase {
@@ -15,21 +16,30 @@ public class FishPage extends PageBase {
     By Goldfish = By.xpath("//*[@id=\"Catalog\"]/table/tbody/tr[5]/td[1]/a");
     By GoldfishProduct = By.xpath("//*[@id=\"Catalog\"]/table/tbody/tr[2]/td[1]/a");
     By addToCart = By.xpath("//*[@id=\"CenterForm\"]/div/a");
-    By Return=By.xpath("//*[@id=\"BackLink\"]/a");
+    By Return = By.xpath("//*[@id=\"BackLink\"]/a");
     By removeFromCart = By.xpath("//*[@id=\"Cart\"]/form/table/tbody/tr[2]/td[8]/a");
-    By proceedCheckout=By.xpath("//*[@id=\"Cart\"]/a");
-    By continueCheckout=By.xpath("//*[@id=\"CenterForm\"]/form/div/button[1]");
-    By confirm =By.xpath("//*[@id=\"CenterForm\"]/form/div/button[1]");
+    By proceedCheckout = By.xpath("//*[@id=\"Cart\"]/a");
+    By continueCheckout = By.xpath("//*[@id=\"CenterForm\"]/form/div/button[1]");
+    By confirm = By.xpath("//*[@id=\"CenterForm\"]/form/div/button[1]");
+    By checkoutTab = By.xpath("//a[@href='/order/newOrderForm']");
+    By categoryHeader = By.xpath("//h3");
+    By itemLocator = By.xpath("//a[contains(@href, 'items')]");
+    By checkoutHeader = By.xpath("/html/body/section/div[2]/div[2]/div/form/h3[1]");
+    By checkoutConfirmation = By.xpath("/html/body/section/div[2]/div[2]/div[1]/p");
+    By totalCheckout = By.xpath("/html/body/section/div[2]/div[2]/div[2]/div[1]/form/table/tbody/tr[4]/td[4]/strong");
+
+    JavascriptExecutor js = (JavascriptExecutor)driver;
 
     public FishPage(WebDriver driver) {
         super(driver);
     }
 
-    public void ClickOnFishPage(){
+    public void ClickOnFishPage() {
         clickOnElement(fishPage);
         scrollDown("window.scrollBy(0,368.79998779296875)");
     }
-    public void AngelFish(){
+
+    public void AngelFish() {
         clickOnElement(Angelfish);
         scrollDown("window.scrollBy(0,368.79998779296875)");
         clickOnElement(AngelfishProduct);
@@ -37,7 +47,8 @@ public class FishPage extends PageBase {
         clickOnElement(addToCart);
         scrollDown("window.scrollBy(0,368.79998779296875)");
     }
-    public void TigerShark(){
+
+    public void TigerShark() {
         clickOnElement(TigerShark);
         scrollDown("window.scrollBy(0,368.79998779296875)");
         clickOnElement(TigerSharkProduct);
@@ -45,7 +56,8 @@ public class FishPage extends PageBase {
         clickOnElement(addToCart);
         scrollDown("window.scrollBy(0,368.79998779296875)");
     }
-    public void Koi(){
+
+    public void Koi() {
         clickOnElement(Koi);
         scrollDown("window.scrollBy(0,368.79998779296875)");
         clickOnElement(KoiProduct);
@@ -53,7 +65,8 @@ public class FishPage extends PageBase {
         clickOnElement(addToCart);
         scrollDown("window.scrollBy(0,368.79998779296875)");
     }
-    public void GoldFish(){
+
+    public void GoldFish() {
         clickOnElement(Goldfish);
         scrollDown("window.scrollBy(0,368.79998779296875)");
         clickOnElement(GoldfishProduct);
@@ -61,22 +74,74 @@ public class FishPage extends PageBase {
         clickOnElement(addToCart);
         scrollDown("window.scrollBy(0,368.79998779296875)");
     }
-    public void Return(){
+
+    public void Return() {
         clickOnElement(Return);
         scrollDown("window.scrollBy(0,368.79998779296875)");
     }
-    public void RemoveFromCart(){
+
+    public void RemoveFromCart() {
         clickOnElement(removeFromCart);
         scrollDown("window.scrollBy(0,368.79998779296875)");
     }
-    public void CheckOut(){
+
+    public void CheckOut() {
         clickOnElement(proceedCheckout);
         scrollDown("window.scrollBy(0,483.20001220703125)");
         scrollDown("window.scrollBy(0,634.4000244140625)");
         clickOnElement(continueCheckout);
         scrollDown("window.scrollBy(0,1028)");
         clickOnElement(confirm);
-        Return();
     }
 
+    public void clickOnCheckoutTab() {
+        scrollDown("window.scrollBy(0,368.79998779296875)");
+        clickOnElement(checkoutTab);
+    }
+
+    public String isCategoryHeaderDisplayed() {
+        return driver.findElement(categoryHeader).getText();
+    }
+
+    public String getCartCount() {
+        return js.executeScript(
+                "return document.evaluate(" +
+                        "'//img[@name=\"img_cart\"]/following-sibling::text()[1]'," +
+                        "document, null, XPathResult.STRING_TYPE, null).stringValue.trim();"
+        ).toString();
+    }
+
+    public String isItemInCart() {
+        return driver.findElement(itemLocator).getText();
+    }
+
+    public String isCheckoutHeaderDisplayed() {
+        return driver.findElement(checkoutHeader).getText();
+    }
+
+    public String isCheckoutComplete() {
+        return driver.findElement(checkoutConfirmation).getText();
+    }
+
+    public String getCartTotal() {
+        return driver.findElement(totalCheckout).getText();
+    }
+
+    public void removeItem(String itemId) {
+        clickOnElement(By.xpath("//a[@hx-post='/cart/removeItemFromCart?cartItem=" + itemId + "']"));
+    }
+
+    public void updateProductQuantity(String itemId, int quantity) {
+        By quantityField = By.xpath("//a[contains(@href, '" + itemId + "')]/../../td[5]/input");
+        By updateButton = By.xpath("/html/body/section/div[2]/div[2]/div[2]/div[1]/form/table/tbody/tr[3]/td[2]/button");
+
+        driver.findElement(quantityField).clear();
+        driver.findElement(quantityField).sendKeys(String.valueOf(quantity));
+        clickOnElement(updateButton);
+    }
+
+    public String getProductQuantity(String itemId) {
+        By quantityField = By.xpath("//a[contains(@href, '" + itemId + "')]/../../td[5]/input");
+        return driver.findElement(quantityField).getAttribute("value");
+    }
 }
--- JPetStore-Demo/src/test/java/tests/FishTest.java
+++ JPetStore-Demo/src/test/java/tests/FishTest.java
@@ -1,53 +1,72 @@
 package tests;
 
 import Helper.LoginHelper;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.FishPage;
-import pages.SignInPage;
 
-public class FishTest extends TestBase{
-
+public class FishTest extends TestBase {
     FishPage fishPage;
     LoginHelper loginHelper;
 
-
     @BeforeMethod
-    public void init(){
+    public void init() {
         loginHelper = new LoginHelper(driver);
         fishPage = new FishPage(driver);
+        loginHelper.login("salem", "Passw0rd123");
     }
 
-    @Test
-    public void FishTestUntilCheckout() throws InterruptedException {
-        System.out.println(driver);
-        loginHelper.login("salem","Passw0rd123");
+    @Test(priority = 1)
+    public void verifyFishCategoriesLoad() {
+        fishPage.ClickOnFishPage();
+        Assert.assertTrue(fishPage.isCategoryHeaderDisplayed().contains("Fish"),
+                "Fish categories page should load properly");
+    }
 
+    @Test(priority = 2)
+    public void testAngelFishAdoption() {
         fishPage.ClickOnFishPage();
-        Thread.sleep(3000);
         fishPage.AngelFish();
-        Thread.sleep(3000);
-        fishPage.Return();
-        Thread.sleep(3000);
+        Assert.assertEquals(fishPage.isItemInCart(), "EST-1");
+    }
+
+    @Test(priority = 3)
+    public void testItemRemoval() throws InterruptedException {
         fishPage.ClickOnFishPage();
-        Thread.sleep(3000);
         fishPage.TigerShark();
+        fishPage.removeItem("EST-3");
         Thread.sleep(3000);
-        fishPage.Return();
-        Thread.sleep(3000);
+        Assert.assertEquals(fishPage.getCartCount(), "0",
+                "Item should be removable from cart");
+    }
+
+    @Test(priority = 4)
+    public void testTotalCalculation() throws InterruptedException {
         fishPage.ClickOnFishPage();
-        Thread.sleep(3000);
         fishPage.Koi();
-        Thread.sleep(3000);
-        fishPage.Return();
-        Thread.sleep(3000);
         fishPage.ClickOnFishPage();
-        Thread.sleep(3000);
         fishPage.GoldFish();
         Thread.sleep(3000);
-        fishPage.RemoveFromCart();
-        Thread.sleep(3000);
-        fishPage.CheckOut();
+        Assert.assertEquals(fishPage.getCartTotal(), "$24.00");
+    }
 
+    @Test(priority = 5)
+    public void testCheckoutProcess() throws InterruptedException {
+        fishPage.ClickOnFishPage();
+        fishPage.AngelFish();
+        fishPage.CheckOut();
+        Thread.sleep(3000);
+        Assert.assertEquals(fishPage.isCheckoutComplete(), "Thank you, your order has been submitted.");
+    }
+
+    @Test(priority = 6)
+    public void testUpdateProductQuantity() throws InterruptedException {
+        fishPage.ClickOnFishPage();
+        fishPage.AngelFish();
+        fishPage.updateProductQuantity("EST-1", 3);
+        Thread.sleep(3000);
+        Assert.assertEquals(fishPage.getProductQuantity("EST-1"), "3",
+                "Product quantity should be updated successfully");
     }
 }
